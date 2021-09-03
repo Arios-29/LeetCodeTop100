971dc52a2dec8ba58d3b7f8d2d464c7e7dbfb703
ChenYu <2508335092@qq.com>
2021-09-03 17:18:42 +0800

    两数相加

--- src/main/java/com/solutions/SolutionTwo.java
+++ src/main/java/com/solutions/SolutionTwo.java
@@ -23,48 +23,48 @@ public class SolutionTwo {
         ListNode cur = head;
         int tmp = 0;
         while (l1 != null && l2 != null) {
-            int sum = l1.val+l2.val+tmp;
-            if(sum>=10){
-                sum=sum-10;
-                tmp=1;
-            }else{
-                tmp=0;
+            int sum = l1.val + l2.val + tmp;
+            if (sum >= 10) {
+                sum = sum - 10;
+                tmp = 1;
+            } else {
+                tmp = 0;
             }
             ListNode node = new ListNode(sum);
-            cur.next=node;
-            cur=cur.next;
-            l1=l1.next;
-            l2=l2.next;
+            cur.next = node;
+            cur = cur.next;
+            l1 = l1.next;
+            l2 = l2.next;
         }
-        while(l1 != null){
-            int sum = l1.val+tmp;
-            if(sum>=10){
-                sum=sum-10;
-                tmp=1;
-            }else{
-                tmp=0;
+        while (l1 != null) {
+            int sum = l1.val + tmp;
+            if (sum >= 10) {
+                sum = sum - 10;
+                tmp = 1;
+            } else {
+                tmp = 0;
             }
             ListNode node = new ListNode(sum);
-            cur.next=node;
-            cur=cur.next;
-            l1=l1.next;
+            cur.next = node;
+            cur = cur.next;
+            l1 = l1.next;
         }
-        while(l2!=null){
-            int sum = l2.val+tmp;
-            if(sum>=10){
-                sum=sum-10;
-                tmp=1;
-            }else{
-                tmp=0;
+        while (l2 != null) {
+            int sum = l2.val + tmp;
+            if (sum >= 10) {
+                sum = sum - 10;
+                tmp = 1;
+            } else {
+                tmp = 0;
             }
             ListNode node = new ListNode(sum);
-            cur.next=node;
-            cur=cur.next;
-            l2=l2.next;
+            cur.next = node;
+            cur = cur.next;
+            l2 = l2.next;
         }
-        if(tmp==1){
+        if (tmp == 1) {
             ListNode node = new ListNode(1);
-            cur.next=node;
+            cur.next = node;
         }
         return head.next;
     }
