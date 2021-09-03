b59a0fc6a254f6e4d2ff824d65c99d4afee277ce
ChenYu <2508335092@qq.com>
2021-09-03 21:36:32 +0800

    寻找两个正序数组的中位数

---
+++ src/main/java/com/solutions/SolutionFour.java
@@ -0,0 +1,38 @@
+package com.solutions;
+
+public class SolutionFour {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int amount = nums1.length + nums2.length;
+        int[] nums = new int[amount];
+        int index = 0;
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] <= nums2[j]) {
+                nums[index] = nums1[i];
+                i++;
+                index++;
+            } else {
+                nums[index] = nums2[j];
+                j++;
+                index++;
+            }
+        }
+        while (i < nums1.length) {
+            nums[index] = nums1[i];
+            i++;
+            index++;
+        }
+        while (j < nums2.length) {
+            nums[index] = nums2[j];
+            j++;
+            index++;
+        }
+        if (amount % 2 == 1) {
+            return (double) nums[amount / 2];
+        } else {
+            double ans = (nums[amount / 2 - 1] + nums[amount / 2]) / 2.0;
+            return ans;
+        }
+    }
+}
