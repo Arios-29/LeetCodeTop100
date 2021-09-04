c34db07d8d38f911707569c6186b3c7ed0bf6159
ChenYu <2508335092@qq.com>
2021-09-04 22:04:42 +0800

    最长回文子串

--- src/main/java/com/solutions/SolutionFive.java
+++ src/main/java/com/solutions/SolutionFive.java
@@ -9,43 +9,38 @@ public class SolutionFive {
         int start = 0; //记录最长的回文串的初始下标
         for (int len = 1; len <= size; len++) {
             for (int i = 0; i + len - 1 < size; i++) {
-               int j=i+len-1;
-               if(len==1){
-                   dp[i][j]=1;
-               }
-               else if(len==2){
-                   if(s.charAt(i)==s.charAt(j)){
-                       dp[i][j]=2;
-                       if(dp[i][j]>max){
-                           max=dp[i][j];
-                           start=i;
-                       }
-                   }
-                   else {
-                       dp[i][j]=-1;
-                   }
-               }
-               else{
-                   if(dp[i+1][j-1]!=-1){
-                       if(s.charAt(i)==s.charAt(j)){
-                           dp[i][j]=dp[i+1][j-1]+2;
-                           if(dp[i][j]>max){
-                               max=dp[i][j];
-                               start=i;
-                           }
-                       }
-                       else{
-                           dp[i][j]=-1;
-                       }
-                   }
-                   else{
-                       dp[i][j]=-1;
-                   }
-               }
+                int j = i + len - 1;
+                if (len == 1) {
+                    dp[i][j] = 1;
+                } else if (len == 2) {
+                    if (s.charAt(i) == s.charAt(j)) {
+                        dp[i][j] = 2;
+                        if (dp[i][j] > max) {
+                            max = dp[i][j];
+                            start = i;
+                        }
+                    } else {
+                        dp[i][j] = -1;
+                    }
+                } else {
+                    if (dp[i + 1][j - 1] != -1) {
+                        if (s.charAt(i) == s.charAt(j)) {
+                            dp[i][j] = dp[i + 1][j - 1] + 2;
+                            if (dp[i][j] > max) {
+                                max = dp[i][j];
+                                start = i;
+                            }
+                        } else {
+                            dp[i][j] = -1;
+                        }
+                    } else {
+                        dp[i][j] = -1;
+                    }
+                }
             }
 
         }
-        return s.substring(start,start+max);
+        return s.substring(start, start + max);
 
     }
 }
