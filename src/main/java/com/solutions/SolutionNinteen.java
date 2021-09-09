43d0cfd589238114d85afe075d6a3da9d4568af4
ChenYu <2508335092@qq.com>
2021-09-09 16:57:27 +0800

    删除链表的倒数第n个节点

--- src/main/java/com/solutions/SolutionNinteen.java
+++ src/main/java/com/solutions/SolutionNinteen.java
@@ -19,22 +19,22 @@ public class SolutionNinteen {
     }
 
     public ListNode removeNthFromEnd(ListNode head, int n) {
-            ListNode pre=new ListNode(-1,head);
-            ListNode cur=head;
-            ListNode end=head;
-            for(int i=1;i<n;i++){
-                end=end.next;
-            }
-            while(end.next!=null){
-                pre=pre.next;
-                cur=cur.next;
-                end=end.next;
-            }
-            if(cur==head){
-                return head.next;
-            }else{
-                pre.next=cur.next;
-                return head;
-            }
+        ListNode pre = new ListNode(-1, head);
+        ListNode cur = head;
+        ListNode end = head;
+        for (int i = 1; i < n; i++) {
+            end = end.next;
+        }
+        while (end.next != null) {
+            pre = pre.next;
+            cur = cur.next;
+            end = end.next;
+        }
+        if (cur == head) {
+            return head.next;
+        } else {
+            pre.next = cur.next;
+            return head;
+        }
     }
 }
