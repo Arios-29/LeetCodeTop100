7dcd8c2d8012633ef0f63d0e4b49d6e589cf4861
ChenYu <2508335092@qq.com>
2021-09-12 17:23:37 +0800

    有效的括号

---
+++ src/main/java/com/solutions/SolutionTwentyOne.java
@@ -0,0 +1,45 @@
+package com.solutions;
+
+/**
+ * @author ChenYu
+ * @date 2021/9/12 16:55
+ */
+public class SolutionTwentyOne {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode();
+        ListNode pre = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                pre.next = l1;
+                l1 = l1.next;
+            } else {
+                pre.next = l2;
+                l2 = l2.next;
+            }
+            pre = pre.next;
+        }
+        if (l1 == null) {
+            pre.next = l2;
+        } else {
+            pre.next = l1;
+        }
+        return head.next;
+    }
+}
